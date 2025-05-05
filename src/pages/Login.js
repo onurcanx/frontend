f96f7dd9448bdf3716adc48ab13cc386e45fb104
onurcanx <onuraa79@gmail.com>
2025-05-05 22:07:01 +0300

    Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/auth/login", {
+      const response = await axios.post(process.env.REACT_APP_API_URL + "/auth/login", {
         usernameOrEmail,
         password,
       });
